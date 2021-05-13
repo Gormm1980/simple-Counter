199dc6bedaa596069a02ce8588b37c63e3d59ec1
Gormm1980 <guillermosantamariarodrigo@hotmail.com>
2021-05-13 20:50:40 +0000

    add contains

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,24 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import PropTypes from "prop-types";
 //create your first component
-export function Home() {
+function Home(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="clock">
+				<i className="far fa-clock"></i>
+				<div className="four">{props.PossFour % 10}</div>
+				<div className="three">{props.PossThree % 10}</div>
+				<div className="two">{props.PossTwo % 10}</div>
+				<div className="one">{props.PossOne % 10}</div>
+			</div>
 		</div>
 	);
 }
+Home.proptypes = {
+	PossOne: PropTypes.number,
+	PossTwo: PropTypes.number,
+	PossThree: PropTypes.number,
+	PossFour: PropTypes.number
+};
+
+export default Home;
